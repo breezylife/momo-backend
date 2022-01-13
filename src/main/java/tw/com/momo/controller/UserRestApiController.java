20c4724d3af693cca38e43ea55c01cf8d324adeb
Johnny <ps102646@gmail.com>
2022-01-13 15:32:08 +0800

    update

--- src/main/java/tw/com/momo/controller/UserRestApiController.java
+++ src/main/java/tw/com/momo/controller/UserRestApiController.java
@@ -10,13 +10,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,9 +24,8 @@ import tw.com.momo.dao.UserRepository;
 import tw.com.momo.domain.ConfirmationTokenBean;
 import tw.com.momo.domain.UserBean;
 import tw.com.momo.payload.request.LoginDto;
-import tw.com.momo.payload.request.PasswordDto;
+import tw.com.momo.payload.request.OauthRequestDto;
 import tw.com.momo.payload.request.SignUpDto;
-import tw.com.momo.payload.request.UserDto;
 import tw.com.momo.payload.response.JwtResponse;
 import tw.com.momo.service.EmailSenderService;
 import tw.com.momo.service.UserDetailsImpl;
@@ -61,8 +57,9 @@ public class UserRestApiController {
 		UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
 			SecurityContextHolder.getContext().setAuthentication(authentication);
 			String jwt = jwtUtils.generateJwtToken(authentication);
-			
+			System.out.println(jwt);
 			return new JwtResponse(jwt, userDetails.getId(), userDetails.getUsername(), userDetails.getEmail());
+			
 	}
 
 //	@RequestMapping(value = "/register", method = RequestMethod.GET)
@@ -71,18 +68,10 @@ public class UserRestApiController {
 //		modelAndView.setViewName("register");
 //		return modelAndView;
 //	}
-	@GetMapping("/user")
-	@CrossOrigin
-	public ResponseEntity<?> readuser() {
-		UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		UserBean user = userRepository.findByUsername(userDetails.getUsername());
-//		System.out.println("123123asdf"+user);
-		return ResponseEntity.ok(user);
-	}
 
-	@PostMapping("/user")
+	@PostMapping("/register")
 	@CrossOrigin
-	public String create(@RequestBody SignUpDto signUpDto) {
+	public String registerUser(@RequestBody SignUpDto signUpDto) {
 		
 		if (userRepository.existsByUsername(signUpDto.getUsername())) {
 //			return new ResponseEntity<>("Username is already taken!", HttpStatus.BAD_REQUEST);\
@@ -122,9 +111,29 @@ public class UserRestApiController {
 //		return new ResponseEntity<>("Plz check the Email to confim your account", HttpStatus.OK);
 		return "{\"success\":1}";
 	}
+	
+	@PostMapping("/Oauth")
+	@CrossOrigin
+	public ResponseEntity<?> oauthRegister(@RequestBody OauthRequestDto oauthRequestDto) {
+		UserBean user = new UserBean();
+		if (userRepository.existsByUsername(oauthRequestDto.getDisplayName())) {
+//			return new ResponseEntity<>("Username is already taken!", HttpStatus.BAD_REQUEST);\
+			return new ResponseEntity<String>("使用者名稱已有人使用", HttpStatus.NOT_FOUND);
+		} else if (userRepository.existsByEmail(oauthRequestDto.getEmail())) {
+//			return new ResponseEntity<>("Email is already taken!", HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<String>("電子郵件已有人使用", HttpStatus.NOT_FOUND);
+		} else {
+			user.setEmail(oauthRequestDto.getEmail());
+			user.setPassword(passwordEncoder.encode(oauthRequestDto.getUid()));
+			user.setUsername(oauthRequestDto.getDisplayName());
+			user.setUserphoto(oauthRequestDto.getPhotoURL());
+			user.setEnabled(true);
+			userRepository.save(user);
+		}
+		return ResponseEntity.created(null).body(user);
+	}
 
 	@GetMapping("/confirm-account")
-	@CrossOrigin
 	public ResponseEntity<?> confirmUserAccount(@RequestParam("token") String confirmationToken) {
 		ConfirmationTokenBean token = confirmationTokenRepository.findByConfirmationToken(confirmationToken);
 		
@@ -153,56 +162,6 @@ public class UserRestApiController {
 
 //		return new ResponseEntity<>("User registered successfully", HttpStatus.OK);
 	}
-	
-	// 修改會員資料
-		@PutMapping("/user")
-		@CrossOrigin
-		public ResponseEntity<?> update(@RequestBody UserDto userDto) {
-			
-			UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-			UserBean userBean = userRepository.findByUsername(userDetails.getUsername());
-			
-//			userBean.setPassword(passwordEncoder.encode(userDto.getPassword()));
-			userBean.setGender(userDto.getGender());
-			userBean.setBirthday(userDto.getBirthday());
-			userBean.setPhone(userDto.getPhone());
-			userBean.setAddress(userDto.getAddress());
-			
-			userRepository.save(userBean);
-
-//			return new ResponseEntity<>("會員資料修改成功", HttpStatus.OK);
-			return ResponseEntity.ok(userBean);
-		}
-		
-		// 修改密碼
-		@PostMapping("/password")
-		@CrossOrigin
-		public String changePassword(@RequestBody PasswordDto passwordDto){
-			System.out.println(passwordDto);
-			UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-			UserBean user = userRepository.findByUsername(userDetails.getUsername());
-							
-			if(passwordEncoder.matches(passwordDto.getOldPassword(),user.getPassword())) {
-				if(passwordDto.getPassword().equals(passwordDto.getPassword2())) {
-					user.setPassword(passwordEncoder.encode(passwordDto.getPassword()));
-					userRepository.save(user);
-//					return new ResponseEntity<>("OKOK", HttpStatus.OK);
-					return "{\"success\":1}";
-				}else {
-					System.out.println("新密碼不一致");
-//					return new ResponseEntity<>("新密碼不一致", HttpStatus.NOT_FOUND);
-					return "{\"error\":2}";
-				}
-				
-			}else {
-				System.out.println("密碼輸入錯誤"+passwordEncoder.matches(user.getPassword(), passwordEncoder.encode(passwordDto.getOldPassword())));
-				System.out.println(user.getPassword()+passwordDto.getOldPassword());
-//				return new ResponseEntity<>("密碼輸入錯誤", HttpStatus.NOT_FOUND);
-				return "{\"error\":1}";
-			}
-		
-			
-		}
 
 //	@PostMapping("/signup")
 //	public ResponseEntity<?> registerUser(@RequestBody SignUpDto signUpDto) {
