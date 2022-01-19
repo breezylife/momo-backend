af41ee3bd150374350f77eab468ec6e158d4b049
Johnny-Lin <ps102646@gmail.com>
2022-01-19 19:12:36 +0800

    update

--- src/main/java/tw/com/momo/domain/ProductBean.java
+++ src/main/java/tw/com/momo/domain/ProductBean.java
@@ -23,7 +23,7 @@ public class ProductBean {
 	private Integer id;
 	private String name;
 	private Integer price;
-	private Integer stock;
+//	private Integer stock;
 //	private Integer num;
 	private String description;
 	private String category;
@@ -51,7 +51,7 @@ public class ProductBean {
 
 	@Override
 	public String toString() {
-		return "ProductBean [id=" + id + ", name=" + name + ", price=" + price + ", stock=" + stock + ", description="
+		return "ProductBean [id=" + id + ", name=" + name + ", price=" + price  + ", description="
 				+ description + ", category=" + category + ", state=" + state + ", createdDate=" + createddate + "]";
 	}
 
@@ -78,15 +78,6 @@ public class ProductBean {
 	public void setPrice(int price) {
 		this.price = price;
 	}
-
-	public int getStock() {
-		return stock;
-	}
-
-	public void setStock(int stock) {
-		this.stock = stock;
-	}
-
 	public String getDescription() {
 		return description;
 	}
