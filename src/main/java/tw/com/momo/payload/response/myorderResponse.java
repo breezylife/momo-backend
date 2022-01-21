e561013e3a14809d75057af1b74b7fda400a3343
joelkecode0112 <83998682+joelkecode0112@users.noreply.github.com>
2022-01-21 13:44:13 +0800

    order update

--- src/main/java/tw/com/momo/payload/response/myorderResponse.java
+++ src/main/java/tw/com/momo/payload/response/myorderResponse.java
@@ -18,6 +18,9 @@ public interface myorderResponse{
 	String getBuyername();
 	Integer getTotal();
 	Integer getPrid();
-	Integer Alltotal();
+	Integer getStock();
+	String getSpec();
 	String getSetuptime();
+	String getCover();
+	
 }
