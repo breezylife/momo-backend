7015e4c87ee3fd338e399ce5053f5ac7e78e77ef
Peggie0657 <86345006+Peggie0657@users.noreply.github.com>
2022-01-21 09:51:25 +0800

    fk

--- src/main/java/SocketIO/MessageStatus.java
+++
@@ -1,5 +0,0 @@
-package SocketIO;
-
-public enum MessageStatus {
-    RECEIVED, DELIVERED
-}
--- src/main/java/tw/com/momo/config/SecurityConfig.java
+++ src/main/java/tw/com/momo/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package tw.com.momo.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -15,6 +14,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import tw.com.momo.filter.JwtAuthenticationFilter;
 
@@ -35,17 +37,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public JwtAuthenticationFilter authenticationJwtTokenFilter() {
 		return new JwtAuthenticationFilter();
 	}
+	
+	@Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("anonymous");
+        config.addAllowedHeader("anonymous");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
 //        		.cors().and()
+        		
         		.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/**").permitAll()
+                .antMatchers("/ws/**").permitAll()
 //              .antMatchers("/api/auth/**").permitAll()
 //              .antMatchers("/api/product/**").permitAll()
 //              .antMatchers("/api/order/**").permitAll()
+                
                 .anyRequest()
                 .authenticated()
                 .and()
